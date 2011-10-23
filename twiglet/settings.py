279dd783abc3e78e196b81a8ef0748ad764fa35b
Kevin Glynn <kevin.glynn@twigletsoftware.com>
2011-10-23 01:53:32 -0700

    move twiglet to webapp module

--- twiglet/settings.py
+++ twiglet/settings.py
@@ -97,7 +97,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
-    'twiglet',
+    'webapp',
 )
 
 ## twiglet settings
--- twiglet/urls.py
+++ twiglet/urls.py
@@ -5,8 +5,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^twiglet/', include('twiglet.foo.urls')),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
@@ -15,25 +13,25 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
 
-    (r'^contact/$', 'twiglet.views.contact'),
-    (r'^contact_thanks/$', 'twiglet.views.contact_thanks'),
-    (r'^download/$', 'twiglet.views.download'),
-    (r'^download_thanks/$', 'twiglet.views.download_thanks'),
-    (r'^download_trial/$', 'twiglet.views.download_trial'),
-    (r'^how/$', 'twiglet.views.how'),
-    (r'^features/$', 'twiglet.views.features'),
-    (r'^showcase/$', 'twiglet.views.showcase'),
-    (r'^showcase/helloworld/$', 'twiglet.views.showcase_helloworld'),
-    (r'^showcase/generics/$', 'twiglet.views.showcase_generics'),
-    (r'^showcase/delegates/$', 'twiglet.views.showcase_delegates'),
-    (r'^showcase/rustici/$', 'twiglet.views.showcase_rustici'),
-    (r'^showcase/rustici/dirtree_help$', 'twiglet.views.showcase_dirtree_help'),
-    (r'^showcase/code_no_license/$', 'twiglet.views.showcase_no_license'),
-    (r'^documentation/$', 'twiglet.views.cs2jdocumentation'),
-    (r'^$', 'twiglet.views.home'),
-    (r'^util/displaytree/(?P<project>\w+)/(?P<fname>(\w|\.)*)/?$', 'twiglet.views.util_display'),
-    (r'^util/displaycode/(?P<formatting>(pretty|plain))/(?P<project>\w+)/(?P<fname>(\w|\.)*)/?$', 'twiglet.views.util_display_code'),
-    (r'^util/download/(?P<project>\w+)/(?P<fname>(\w|\.)*)/?$', 'twiglet.views.util_download'),
+    (r'^contact/$', 'webapp.views.contact'),
+    (r'^contact_thanks/$', 'webapp.views.contact_thanks'),
+    (r'^download/$', 'webapp.views.download'),
+    (r'^download_thanks/$', 'webapp.views.download_thanks'),
+    (r'^download_trial/$', 'webapp.views.download_trial'),
+    (r'^how/$', 'webapp.views.how'),
+    (r'^features/$', 'webapp.views.features'),
+    (r'^showcase/$', 'webapp.views.showcase'),
+    (r'^showcase/helloworld/$', 'webapp.views.showcase_helloworld'),
+    (r'^showcase/generics/$', 'webapp.views.showcase_generics'),
+    (r'^showcase/delegates/$', 'webapp.views.showcase_delegates'),
+    (r'^showcase/rustici/$', 'webapp.views.showcase_rustici'),
+    (r'^showcase/rustici/dirtree_help$', 'webapp.views.showcase_dirtree_help'),
+    (r'^showcase/code_no_license/$', 'webapp.views.showcase_no_license'),
+    (r'^documentation/$', 'webapp.views.cs2jdocumentation'),
+    (r'^$', 'webapp.views.home'),
+    (r'^util/displaytree/(?P<project>\w+)/(?P<fname>(\w|\.)*)/?$', 'webapp.views.util_display'),
+    (r'^util/displaycode/(?P<formatting>(pretty|plain))/(?P<project>\w+)/(?P<fname>(\w|\.)*)/?$', 'webapp.views.util_display_code'),
+    (r'^util/download/(?P<project>\w+)/(?P<fname>(\w|\.)*)/?$', 'webapp.views.util_download'),
 
 
 )
