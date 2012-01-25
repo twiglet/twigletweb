bf80cb199eeef2e1d5d287eabd6488b6dcafed17
Kevin Glynn <kevin.glynn@twigletsoftware.com>
2012-01-25 06:36:55 +0100

    pygments barfs on copyright symbol

--- assets/cs2j/showcase/code/fizzbuzz/csharp/FizzBuzz/SolutionInfo.cs
+++ assets/cs2j/showcase/code/fizzbuzz/csharp/FizzBuzz/SolutionInfo.cs
@@ -4,6 +4,6 @@ using System.Reflection;
 [assembly: AssemblyConfiguration("Enterprise")]
 [assembly: AssemblyCompany("Wolfbyte Productions")]
 [assembly: AssemblyProduct("FizzBuzz")]
-[assembly: AssemblyCopyright("Copyright ©  2007")]
+[assembly: AssemblyCopyright("Copyright (c) 2007")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
